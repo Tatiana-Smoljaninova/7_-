25e60048b46ff84db59c7069d67a10db0f59ea22
Tatiana <tsmall@rambler.ru>
2023-02-17 17:53:19 +0600

    7-Двумерные массивы

---
+++ Домашнее_Задача_50/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в 
+//двумерном массиве, и возвращает значение этого элемента или же указание, что такого 
+//элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+Console.Write("Введите число поиска: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+int[,] array = new int[5,4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(1,10);
+        Console.Write($"{array[i,j]} ");
+        if (array[i,j] == num)
+        {
+            count++;
+        }
+    }
+    Console.WriteLine();
+}
+
+if (count == 0)
+{
+     Console.WriteLine("Такого числа в массиве нет");
+}
+else 
+{
+    Console.WriteLine($"Значение {num} найдено");
+}
---
+++ Семинар_Задача_49/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 49: Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные,
+//и замените эти элементы на их квадраты.
+//Например, изначально массив выглядел вот так:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Новый будет выглядеть так: 
+//1 4 7 2 
+//5 81 2 9 
+//8 4 2 4
+
+double[,] array = new double[3,4];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i,j] = new Random().Next(1,10); 
+        Console.Write($"{array[i,j]} ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+   {
+    if (i % 2 == 0 && j % 2 == 0)
+        {
+            array[i,j] = Math.Pow(array[i,j],2);
+            Console.Write($"{array[i,j]} ");
+        }
+        else
+        {
+            array[i,j] = array[i,j];
+            Console.Write($"{array[i,j]} ");
+        }
+   }
+   Console.WriteLine();
+}
